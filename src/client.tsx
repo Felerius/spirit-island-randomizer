0b329beb06617dc54ec09521d7a9dfaba4bbf130
David Stangl <david@david-stangl.com>
2025-08-16 21:26:14 +0200

    Changes required by tanstack start updates

--- src/client.tsx
+++
@@ -1,7 +0,0 @@
-import { createRouter } from "$/router";
-import { StartClient } from "@tanstack/react-start";
-import { hydrateRoot } from "react-dom/client";
-
-const router = createRouter();
-
-hydrateRoot(document, <StartClient router={router} />);
