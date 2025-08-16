22f599a6e25eafe1ea14f919126895002b1e2daa
David Stangl <david@david-stangl.com>
2025-08-16 23:33:37 +0200

    Use zod mini

--- src/data/complexities.ts
+++ src/data/complexities.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import * as z from "zod/mini";
 
 export type ComplexityId = keyof typeof COMPLEXITIES_RAW;
 
--- src/routes/randomize.tsx
+++ src/routes/randomize.tsx
@@ -1,18 +1,28 @@
 import { createFileRoute, Navigate } from "@tanstack/react-router";
 import { sample, shuffle } from "remeda";
-import z from "zod";
+import * as z from "zod/mini";
 import type { AspectId } from "$data/aspects";
 import { COMPLEXITY_IDS, COMPLEXITY_SCHEMA } from "$data/complexities";
 import { EXPANSION_IDS, EXPANSION_SCHEMA } from "$data/expansions";
 import { availableSpirits, SPIRITS, type SpiritId } from "$data/spirits";
 
-const searchSchema = z.object({
-  players: z.string().array().min(1).catch(["Player 1", "Player 2"]),
-  spiritsPerPlayer: z.number().int().min(1).catch(2),
-  chooseAspects: z.boolean().catch(true),
-  expansions: z.array(EXPANSION_SCHEMA).catch(EXPANSION_IDS),
-  complexities: z.array(COMPLEXITY_SCHEMA).catch(COMPLEXITY_IDS),
-});
+const defaultSearchValues = {
+  players: ["Player 1", "Player 2"],
+  spiritsPerPlayer: 2,
+  chooseAspects: true,
+  expansions: EXPANSION_IDS,
+  complexities: COMPLEXITY_IDS,
+};
+const searchSchema = z.catch(
+  z.object({
+    players: z.array(z.string()).check(z.minLength(1)),
+    spiritsPerPlayer: z.int().check(z.minimum(1)),
+    chooseAspects: z.boolean(),
+    expansions: z.array(EXPANSION_SCHEMA),
+    complexities: z.array(COMPLEXITY_SCHEMA),
+  }),
+  defaultSearchValues,
+);
 
 export const Route = createFileRoute("/randomize")({
   validateSearch: searchSchema,
