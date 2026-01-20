6ec5a6f731d9c45f756b10faf223f4cdf3a62894
David Stangl <david@david-stangl.com>
2026-01-20 22:53:58 +0100

    Add icon for base game (#57)

--- src/data/expansions.ts
+++ src/data/expansions.ts
@@ -1,5 +1,6 @@
 import type { FunctionComponent } from "react";
 import * as z from "zod/mini";
+import BaseGameIcon from "$assets/expansions/basegame.svg?react";
 import BranchClawIcon from "$assets/expansions/branchclaw.svg?react";
 import FeatherFlameIcon from "$assets/expansions/featherflame.svg?react";
 import HorizonsIcon from "$assets/expansions/horizons.svg?react";
@@ -8,13 +9,13 @@ import NatureIncarnateIcon from "$assets/expansions/natureincarnate.svg?react";
 
 export interface Expansion {
   name: string;
-  icon?: FunctionComponent;
+  icon: FunctionComponent;
 }
 
 export type ExpansionId = keyof typeof EXPANSIONS_RAW;
 
 const EXPANSIONS_RAW = {
-  base: { name: "Base Game" },
+  base: { name: "Base Game", icon: BaseGameIcon },
   "branch-claw": { name: "Branch & Claw", icon: BranchClawIcon },
   "feather-flame": { name: "Feather & Flame", icon: FeatherFlameIcon },
   horizons: { name: "Horizons of Spirit Island", icon: HorizonsIcon },
